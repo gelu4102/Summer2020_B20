b5ed3e65f6d111dc76584f24b0c438bb263f7e02
gelu4102 <sabukuk@gmail.com>
2020-09-23 19:16:40 -0500

    Day57_Polymorphism

---
+++ src/day57_Polymorphism/AndroidApp.java
@@ -0,0 +1,10 @@
+package day57_Polymorphism;
+/*
+2. create an interface named AndroidApp that can inherit from Downloadable
+            variable: AppStoreName
+ */
+public interface AndroidApp extends Downloadable {
+
+    static String AppStoreName = "Google Play";//by default it is public static final
+    //1 abstract method
+}
---
+++ src/day57_Polymorphism/Downloadable.java
@@ -0,0 +1,10 @@
+package day57_Polymorphism;
+/*
+1. create an interface named Downloadable:
+            abstract method: download();
+ */
+public interface Downloadable {
+
+    void download();
+
+}
